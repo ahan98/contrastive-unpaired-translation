ed722c52e62525cd0211e640f3ec89b7dca0eb73
Alex Han <ahan98@gmail.com>
2020-08-16 12:57:45 -0700

    residual block unit test added and passed

--- models/residual_block.py
+++ models/residual_block.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 class ResidualBlock(nn.Module):
 
     # (3x3 Convolution)-(BatchNorm)-(ReLU)-(3x3 Convolution)-(BatchNorm)
-    def __init__(self, in_channels, batch_momentum):
+    def __init__(self, in_channels, batch_momentum=0.1):
         super().__init__()
-        self.conv_bn1 = conv_bn(in_channels, batch_momentum)
+        self.conv_bn1 = conv_bn(in_channels, 256, batch_momentum)
         self.relu = nn.ReLU()
-        self.conv_bn2 = conv_bn(in_channels, batch_momentum)
+        self.conv_bn2 = conv_bn(256, in_channels, batch_momentum)
 
     def forward(self, x):
         in_channels = x.shape[1]
@@ -19,8 +19,9 @@ class ResidualBlock(nn.Module):
         out += residual
         return out
 
-def conv_bn(in_channels, batch_momentum=0.1):
+def conv_bn(in_channels, out_channels, batch_momentum):
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels=256, kernel_size=3, stride=1),
-        nn.BatchNorm2d(num_features=256, momentum=batch_momentum)
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1,
+                  padding_mode="reflect"),
+        nn.BatchNorm2d(num_features=out_channels, momentum=batch_momentum)
     )
---
+++ tests/test_residual_block.py
@@ -0,0 +1,42 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+import torch
+from models.residual_block import ResidualBlock
+
+def make_residual_block():
+    in_N, in_C, in_H, in_W = 2, 3, 256, 256  # input shape
+    res_block_fn = ResidualBlock(in_C)
+    sample_in = torch.zeros((in_N, in_C, in_H, in_W))
+    res_block = res_block_fn(sample_in)
+    out_N, out_C, out_H, out_W = res_block.shape
+
+    if out_N != in_N:
+        raise Exception(
+            ("Output batch size must match input batch size.\n"
+             + "Input shape: {bs_in}\n"
+             + "Output shape: {bs_out}")
+            .format(bs_in=in_N, bs_out=out_N)
+        )
+    if out_H != in_H:
+        raise Exception(
+            ("Output image height must match input image height.\n"
+             + "Input image height: {h_in}\n"
+             + "Output image height: {h_out}")
+            .format(h_in=in_H, h_out=out_H)
+        )
+    if out_W != in_W:
+        raise Exception(
+            ("Output image width must match input image width.\n"
+             + "Input image width: {w_in}\n"
+             + "Output image width: {w_out}")
+            .format(h_in=in_W, h_out=out_W)
+        )
+
+    print("Residual block test passed")
+    return True
+
+if __name__ == "__main__":
+    make_residual_block()
