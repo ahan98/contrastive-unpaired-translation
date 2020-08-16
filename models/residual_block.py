f9c825ebe2f5a4deff15bb1c02ea2fd2ac3f5f07
Alex Han <ahan98@gmail.com>
2020-08-16 14:33:15 -0700

    refactor and pass test

--- models/residual_block.py
+++ models/residual_block.py
@@ -1,27 +1,20 @@
-import torch
 import torch.nn as nn
+from .helpers import conv_block
 
 class ResidualBlock(nn.Module):
 
     # (3x3 Convolution)-(BatchNorm)-(ReLU)-(3x3 Convolution)-(BatchNorm)
-    def __init__(self, in_channels, batch_momentum=0.1):
+    def __init__(self, batch_momentum=0.1):
         super().__init__()
-        self.conv_bn1 = conv_bn(in_channels, 256, batch_momentum)
-        self.relu = nn.ReLU()
-        self.conv_bn2 = conv_bn(256, in_channels, batch_momentum)
+        self.conv_bn_relu = conv_block(batch_momentum=batch_momentum)
+        self.conv_bn = conv_block(in_channels=256, out_channels=3,
+                                  batch_momentum=batch_momentum, relu=False)
 
     def forward(self, x):
         in_channels = x.shape[1]
         residual = x
-        out = self.conv_bn1(x)
-        out = self.relu(out)
-        out = self.conv_bn2(out)
+        out = self.conv_bn_relu(x)
+        out = self.conv_bn(out)
         out += residual
         return out
 
-def conv_bn(in_channels, out_channels, batch_momentum):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1,
-                  padding_mode="reflect"),
-        nn.BatchNorm2d(num_features=out_channels, momentum=batch_momentum)
-    )
