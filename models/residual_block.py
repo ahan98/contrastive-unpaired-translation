60f52803aab044592977632ae52c49a759dbd70a
Alex Han <ahan98@gmail.com>
2020-08-16 09:49:01 -0700

    Johnson et al. residual block

---
+++ models/residual_block.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+
+    # (3x3 Convolution)-(BatchNorm)-(ReLU)-(3x3 Convolution)-(BatchNorm)
+    def __init__(self, in_channels, batch_momentum):
+        super().__init__()
+        self.conv_bn1 = conv_bn(in_channels, batch_momentum)
+        self.relu = nn.ReLU()
+        self.conv_bn2 = conv_bn(in_channels, batch_momentum)
+
+    def forward(self, x):
+        in_channels = x.shape[1]
+        residual = x
+        out = self.conv_bn1(x)
+        out = self.relu(out)
+        out = self.conv_bn2(out)
+        out += residual
+        return out
+
+def conv_bn(in_channels, batch_momentum=0.1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels=256, kernel_size=3, stride=1),
+        nn.BatchNorm2d(num_features=256, momentum=batch_momentum)
+    )
