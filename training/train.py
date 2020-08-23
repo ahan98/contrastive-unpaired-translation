fixed loss average and print iters

training/train.py
@@ -47,23 +47,20 @@ def train(X_dataloader, Y_dataloader, device="cpu", n_epochs=400, lr=2e-3,
 
     # dictionary of average minibatch loss per epoch
     loss_histories = {"discriminator": [], "generator": [], "patchNCE": []}
-    batch_size = len(X_dataloader)
+    n_iter = 0
 
     for epoch in tqdm(range(n_epochs)):
         print("Epoch {}/{}".format(epoch, n_epochs))
 
-        batch_loss_D = batch_loss_G = batch_loss_P = 0
-
-        for n_batch, real_X in tqdm(enumerate(X_dataloader)):
+        for n_batch, real_X in enumerate(X_dataloader):
             real_X = real_X.to(device)
+            batch_size = len(real_X)
 
             # train discriminator
             loss_D = GANTrainer.train_discriminator(G, D, solver_D, real_X, device)
-            batch_loss_D += loss_D
 
             # train generator
             loss_G, fake_X = GANTrainer.train_generator(G, D, solver_G, real_X.shape, device)
-            batch_loss_G += loss_G
 
             # train PatchNCE
             loss_P = PatchNCETrainer.train_patchnce(P, solver_P, real_X, fake_X, device)
@@ -83,15 +80,15 @@ def train(X_dataloader, Y_dataloader, device="cpu", n_epochs=400, lr=2e-3,
             noise = torch.randn(real_Y.shape, device=device)
             fake_Y = G(noise)
             loss_P += PatchNCETrainer.train_patchnce(P, solver_P, real_Y, fake_Y, device)
-            batch_loss_P += loss_P
 
-        loss_histories["discriminator"] = batch_loss_D / batch_size
-        loss_histories["generator"] = batch_loss_G / batch_size
-        loss_histories["patchNCE"] = batch_loss_P / batch_size
+            loss_histories["discriminator"].append(loss_D / batch_size)
+            loss_histories["generator"].append(loss_G / batch_size)
+            loss_histories["patchNCE"].append(loss_P / batch_size)
 
-        if n_batch % print_every == 0:
-            print("loss_D: {:e}, loss_G: {:e}, loss_P: {:e}"
-                  .format(loss_D, loss_G, loss_P))
+            n_iter += 1
+            if n_iter % print_every == 0:
+                print("loss_D: {:e}, loss_G: {:e}, loss_P: {:e}"
+                      .format(loss_D, loss_G, loss_P))
 
     return D, G, P, loss_histories
 