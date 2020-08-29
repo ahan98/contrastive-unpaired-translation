9333a03a97072fa4eabae36b58456575fc777828
Alex Han <ahan98@gmail.com>
2020-08-28 21:17:53 -0700

    debug

--- models/blocks/ActivationLayer.py
+++ models/blocks/ActivationLayer.py
@@ -1,20 +1,19 @@
 import torch.nn as nn
 from .types import ActivationType
 
+
 class ActivationLayer(nn.Module):
     def __init__(self, activation_type):
         super().__init__()
 
         if activation_type == ActivationType.RELU:
-            model = nn.ReLU()
+            self.model = nn.ReLU()
         elif activation_type == ActivationType.TANH:
-            model = nn.Tanh()
+            self.model = nn.Tanh()
         elif activation_type == ActivationType.LEAKY_RELU:
-            model = nn.LeakyReLU(0.2)
+            self.model = nn.LeakyReLU(0.2)
         elif activation_type == ActivationType.NONE:
-            model = None
-
-        self.model = model
+            self.model = None
 
     def forward(self, x):
         if self.model is None:
